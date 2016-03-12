3b3e7b478f4e4b7778f3032997d036d1126c4678
hirr08 <hirr08@gmail.com>
2016-03-12 13:49:50 -0500

    Created by Coding Ground Client

---
+++ New Project-20160312/Hiral.java
@@ -0,0 +1,12 @@
+
+public class Hiral {
+    
+    public static void main (String args[]){
+        String string1 = ("World");
+        String[] arr = {"one", "two"};
+        StringDemo string2 = new StringDemo(arr);
+        System.out.println(string1);
+    }
+}    
+        
+    
